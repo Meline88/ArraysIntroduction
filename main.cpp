a4c6832a43204cc648bfbfed8168decdad64e6f9
Meline88 <73695389+Meline88@users.noreply.github.com>
2020-12-06 00:16:29 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,14 @@ int main() {
     for (i = 0; i < N; i ++) {
       cin>>arr[i];
     } 
-    for (i = N-1; i >=0 ; i --) {
-      cout<<arr[i]<<" ";
+    for (i = 0; i < N/2; i ++) {
+      int temp;
+      temp = arr[i];
+      arr[i] = arr[N-1-i];
+      arr[N-1-i] = temp;
+    }
+    for (i = 0; i < N; i ++) {
+    cout << arr[i] <<" ";
     }
     delete []arr;
     return 0;
